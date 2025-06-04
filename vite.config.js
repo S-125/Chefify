2007aa6212b021090b011d4dbae9f1ae8c822989
S-125 <sritama813@gmail.com>
2025-06-04 12:22:09 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,6 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/s-125/',
+  base: '/Chefify/',
   plugins: [react(),tailwindcss(),],
 })
